127074f1ec48fe2a87d393aa45c4fec6efd3f58a
disitec <fjmartinez@disitec.es>
2017-01-17 12:09:00 +0100

    add vmodel for validation purposes. emit empty object when select placeholder

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -9,7 +9,8 @@ export default {
   },
   data () {
     return {
-      items: []
+      items: [],
+      selected: ''
     }
   },
   props: {
@@ -49,7 +50,7 @@ export default {
       let selectedItem = this.items.find(item => item[this.keyField] == id)
 
       this.$emit('input', id)
-      this.$emit('selected', selectedItem)
+      this.$emit('selected', selectedItem ? selectedItem : {})
     }
   },
   watch: {
@@ -63,6 +64,7 @@ export default {
 <template>
     <framework-wrapper :fw="frameworkCss">
         <select
+                v-model="selected"
                 @change="updateValue"
                 :class="{ 'form-group' : frameworkCss == 'bootstrap' }"
         >
